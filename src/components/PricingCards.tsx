2db91da86b600f85ac641fe99c135fd532f129fb
Ruth Temporal <ruthtemp@gmail.com>
2022-10-23 13:06:46 +0200

    added images with equal proportions

--- src/components/PricingCards.tsx
+++ src/components/PricingCards.tsx
@@ -1,8 +1,8 @@
 import { Button, Card, Col, Row } from "react-bootstrap";
 import { Check } from "react-bootstrap-icons";
-import img1 from "../media/1.svg";
-import img2 from "../media/1.svg";
-import img3 from "../media/1.svg";
+import img1 from "../media/1.png";
+import img2 from "../media/2.png";
+import img3 from "../media/3.png";
 
 const pricingPLans = [
   {
@@ -56,22 +56,25 @@ export const PricingCards = () => {
     <Row className="mt-4">
       {pricingPLans.map((plan) => (
         <Col sm={4} lg={4} xxl={{ span: 3, offset: 1 }} className="mb-2">
-          <Card className="h-100">
-            <Card.Img src={plan.img} className="mt-3" />
-            <Card.Body className="d-flex flex-column">
-              <span>{plan.preTitle}</span>
-              <Card.Title className="h5">{plan.title}</Card.Title>
-              <Card.Subtitle>subtitle</Card.Subtitle>
-              <span className="mb-3">per month</span>
-              <div>
+          <Card className="h-100 shadow" role="button">
+            <Card.Img src={plan.img} className="mt-3 img-top flex-grow-1" />
+            <Card.Body className="d-flex flex-column ms-2 flex-grow-0">
+              <p>{plan.preTitle}</p>
+              <Card.Title className="fs-3 ">{plan.title}</Card.Title>
+              <Card.Subtitle className="fs-6 mb-3 fw-lighter">
+                per month
+              </Card.Subtitle>
+
+              <Card.Text>
                 {plan.featureList.map((feature) => (
                   <p style={{ color: !feature.included ? "lightgrey" : "" }}>
                     <Check size={30} /> {feature.feature}
                   </p>
                 ))}
+              </Card.Text>
+              <div className="d-flex justify-content-center">
+                <Button className="w-75">{plan.actionText}</Button>
               </div>
-
-              <Button className="w-75">{plan.actionText}</Button>
             </Card.Body>
           </Card>
         </Col>
