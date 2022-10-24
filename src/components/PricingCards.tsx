57f3eabb0d23d0089edec1720e61eb911e696e02
Ruth Temporal <ruthtemp@gmail.com>
2022-10-24 16:47:55 +0200

    gradient background

--- src/components/PricingCards.tsx
+++ src/components/PricingCards.tsx
@@ -2,11 +2,11 @@ import { Col, Row } from "react-bootstrap";
 import { pricingPlans } from "../App";
 import { PlanCard } from "./PlanCard";
 
-export const PricingCards = () => {
+export const PricingPage = () => {
   return (
     <Row className="mt-4">
       {pricingPlans.map((plan) => (
-        <Col sm={4} lg={4} className="mb-2">
+        <Col lg={4} className="mb-2">
           <PlanCard plan={plan} />
         </Col>
       ))}
